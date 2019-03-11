1e9feea91be3818da110e75e91d0264161c99a65
amark_wong <amark.wong@gmail.com>
2019-03-11 19:16:11 +1000

    Updates

---
+++ precache-manifest.d692cb6b439791ed9911f357fa3b56ff.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6ca7def44a7f6e04052",
+    "url": "/weather/static/css/main.c0163413.chunk.css"
+  },
+  {
+    "revision": "a6ca7def44a7f6e04052",
+    "url": "/weather/static/js/main.a6ca7def.chunk.js"
+  },
+  {
+    "revision": "5346c2795f64f3102fab",
+    "url": "/weather/static/js/runtime~main.5346c279.js"
+  },
+  {
+    "revision": "2ec03b31126a4b7a949b",
+    "url": "/weather/static/css/2.cdc41917.chunk.css"
+  },
+  {
+    "revision": "2ec03b31126a4b7a949b",
+    "url": "/weather/static/js/2.2ec03b31.chunk.js"
+  },
+  {
+    "revision": "45b301053b9a76658a4b00d9044275c8",
+    "url": "/weather/static/media/weather-uv.45b30105.png"
+  },
+  {
+    "revision": "c0ec544b81e29648a25cfa9fb8cf4e5d",
+    "url": "/weather/static/media/weather-humidity.c0ec544b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/weather/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/weather/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/weather/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/weather/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/weather/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "874c942a597a0ef4eece0abf35088a00",
+    "url": "/weather/index.html"
+  }
+];
